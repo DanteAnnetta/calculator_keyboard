02a20331d3f511dc739a67e178b4423b331b6c67
Dante Annetta <danteannetta03@gmail.com>
2025-03-03 18:30:43 -0300

    Update arduino_uno.ino

--- arduino_uno/arduino_uno.ino
+++ arduino_uno/arduino_uno.ino
@@ -1,42 +1,44 @@
 #include <Keypad.h>
 #include <Wire.h>
 
-const int rows = 3;
-const int columns = 6;
+// TODO: ACTUALIZAR PARA LEER ÚNICAMENTE UNA MATRIZ DE 2X3 Y EL TRACKPAD, PARA ENVIAR DICHA INFORMACIÓN AL ARDUINO PRO MICRO
+
+
+const byte ROWS = 3;
+const byte COLS = 6;
+
+const byte ROWS_2 = 2;
+const byte COLS_2 = 4;
+
+
 char key;
 
 
-// TODO: change E for (x,n,t)'s key
 
-// b = "TAB" , ¿ = "DELETE" 
-char keys [rows][columns] = {
-  {'h', 'a', 'E', 'v', 'b', '¿'},
+// Mapeo de teclas (ajustar según necesidad)
+char keys[ROWS][COLS] = {
+  {'h', 'a', 'x', 'v', '\t', 127},  // '\t' para TAB, 127 para DELETE
   {'x', 'n', 'l', 'i', ',', '{'},
   {'s', 'c', 't', 'p', 'r', 'q'}
 };
-  
 
-byte rowPins[rows] = {0 , 3 , 2};
-byte columnPins[columns] = {4 , 5 , 6 , 7 , 8 , 9};
+byte rowPins[ROWS] = {0, 2, 3};
+byte colPins[COLS] = {4, 5, 6, 7, 8, 9};
 
-Keypad keypad = Keypad(makeKeymap(keys ) , rowPins , columnPins , rows , columns);
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
-  Wire.begin();
-  //Serial.begin(9600);
-  
-
+    Wire.begin();
+    Serial.begin(9600);
 }
 
 void loop() {
-  
-
-  key = keypad.getKey();
-  if(key != 0){
-    Wire.beginTransmission(8);
-    Wire.write(key);
-    Wire.endTransmission();
-    //Serial.println(key);
-  }
-  delay(10);
+    key = keypad.getKey();
+    
+    if (key) { // Solo enviar si hay una tecla presionada
+        Wire.beginTransmission(8); // Dirección del esclavo I2C
+        Wire.write(key);
+        Wire.endTransmission();
+        Serial.println(key); // Para depuración
+    }
 }
