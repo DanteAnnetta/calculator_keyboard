743ac3f4555fbb854c3dc8975f9bff58323a4ed0
Dante Annetta <danteannetta03@gmail.com>
2025-03-23 20:36:45 -0300

    Update arduino_uno.ino

--- arduino_uno/arduino_uno.ino
+++ arduino_uno/arduino_uno.ino
@@ -1,44 +1,63 @@
 #include <Keypad.h>
 #include <Wire.h>
 
-// TODO: ACTUALIZAR PARA LEER ÚNICAMENTE UNA MATRIZ DE 2X3 Y EL TRACKPAD, PARA ENVIAR DICHA INFORMACIÓN AL ARDUINO PRO MICRO
+// TODO: CAMBIAR SÍMBOLOS DE LA MATRIZ
 
-
-const byte ROWS = 3;
-const byte COLS = 6;
-
-const byte ROWS_2 = 2;
-const byte COLS_2 = 4;
-
+const byte ROWS = 2;
+const byte COLS = 3;
 
 char key;
+String message;
 
-
-
-// Mapeo de teclas (ajustar según necesidad)
+// Mapeo de teclas
 char keys[ROWS][COLS] = {
-  {'h', 'a', 'x', 'v', '\t', 127},  // '\t' para TAB, 127 para DELETE
-  {'x', 'n', 'l', 'i', ',', '{'},
-  {'s', 'c', 't', 'p', 'r', 'q'}
+  {'\t', 127, ','},  // '\t' para TAB, 127 para DELETE
+  {'{', 'r', 'q'}
 };
 
-byte rowPins[ROWS] = {0, 2, 3};
-byte colPins[COLS] = {4, 5, 6, 7, 8, 9};
+byte rowPins[ROWS] = {7, 8};
+byte colPins[COLS] = {9, 10, 11};
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
-    Wire.begin();
-    Serial.begin(9600);
+    Wire.begin();  // Inicializa I2C como maestro
+    //Serial.begin(9600);
+    
+    // Configuración de pines del trackpad
+    for (byte i = 2; i <= 6; i++) {
+        pinMode(i, INPUT_PULLUP);
+    }
+}
+
+String encodeTrackpad() {
+    String pins = "";
+    for (byte i = 2; i <= 6; i++) {
+        pins += String(not(digitalRead(i)));
+        if (i < 6) pins += "_";  // Separador
+    }
+    return pins;
+}
+
+void sendI2CMessage(String msg) {
+    Wire.beginTransmission(8);  // Dirección del esclavo I2C
+    Wire.write(msg.c_str(), msg.length());
+    Wire.endTransmission();
 }
 
 void loop() {
     key = keypad.getKey();
     
     if (key) { // Solo enviar si hay una tecla presionada
-        Wire.beginTransmission(8); // Dirección del esclavo I2C
-        Wire.write(key);
-        Wire.endTransmission();
-        Serial.println(key); // Para depuración
+        message = String(key) + "_" + encodeTrackpad();
+        sendI2CMessage(message);
+        //Serial.println(message); // Para depuración
+    } 
+    else {  // En caso de que no se esté pulsando ninguna tecla del keypad
+        message = String('|') + "_" + encodeTrackpad();  // CARACTER NULO 
+        sendI2CMessage(message);
+        //Serial.println(message); // Para depuración
     }
+
+    delay(100);
 }
